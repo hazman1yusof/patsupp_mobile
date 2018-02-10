dec8a899eb1221eed65471a7515f245c700480cd
hazman1yusof <hazman.yusof@gmail.com>
2018-02-10 19:32:24 +0800

    initial commit

---
+++ app/Http/Controllers/AgentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\agent;
+use Illuminate\Http\Request;
+
+class AgentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\agent  $agent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(agent $agent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\agent  $agent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(agent $agent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\agent  $agent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, agent $agent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\agent  $agent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(agent $agent)
+    {
+        //
+    }
+}
---
+++ app/agent_category.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class agent_category extends Model
+{
+    //
+}
