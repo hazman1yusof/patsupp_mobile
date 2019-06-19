6773ed811995e8e12b83dbc079f4001556ac91ed
hazman1yusof <hazman.yusof@gmail.com>
2019-06-19 10:22:44 +0800

    asd

--- public/js/preview.js
+++ public/js/preview.js
@@ -119,13 +119,20 @@ $(document).ready(function () {
 
     $("#bioage").text(getAge($('#biodob').text()));
     function getAge(dateString) {
-        var today = new Date();
-        var birthDate = new Date(dateString);
-        var age = today.getFullYear() - birthDate.getFullYear();
-        var m = today.getMonth() - birthDate.getMonth();
-        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-            age--;
-        }
+        var datedob = moment(dateString,"DD-MM-YYYY");
+        var today = moment();
+        var age = today.diff(datedob, 'years');
         return age;
     }
+    
+    // function getAge(dateString) {
+    //     var today = new Date();
+    //     var birthDate = new Date(dateString);
+    //     var age = today.getFullYear() - birthDate.getFullYear();
+    //     var m = today.getMonth() - birthDate.getMonth();
+    //     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    //         age--;
+    //     }
+    //     return age;
+    // }
 });
